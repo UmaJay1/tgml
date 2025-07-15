075cd8b6b718f96c8a43c767c59e9dd0fca582d7
Uma <121251148+UmaJay1@users.noreply.github.com>
2025-07-15 11:22:40 -0700

    Updated dependencies, add rs1.py

---
+++ src/rs1.py
@@ -0,0 +1,114 @@
+# https://huggingface.co/docs/transformers/tasks/summarization
+
+from huggingface_hub import notebook_login
+from datasets import load_dataset
+from transformers import AutoTokenizer
+import evaluate
+import numpy as np
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+
+
+billsum = load_dataset("billsum", split="ca_test")
+
+billsum = billsum.train_test_split(test_size=0.2)
+
+# print(billsum["train"][0])
+
+checkpoint = "google-t5/t5-small"
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+prefix = "summarize: "
+
+
+def preprocess_function(examples):
+
+    inputs = [prefix + doc for doc in examples["text"]]
+
+    model_inputs = tokenizer(inputs, max_length=1024, truncation=True)
+
+    labels = tokenizer(text_target=examples["summary"], max_length=128, truncation=True)
+
+    model_inputs["labels"] = labels["input_ids"]
+
+    return model_inputs
+
+
+tokenized_billsum = billsum.map(preprocess_function, batched=True)
+
+from transformers import DataCollatorForSeq2Seq
+
+data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=checkpoint)
+
+rouge = evaluate.load("rouge")
+
+
+def compute_metrics(eval_pred):
+
+    predictions, labels = eval_pred
+
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    result = rouge.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in predictions]
+
+    result["gen_len"] = np.mean(prediction_lens)
+
+    return {k: round(v, 4) for k, v in result.items()}
+
+
+model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+
+training_args = Seq2SeqTrainingArguments(
+
+    output_dir="my_awesome_billsum_model",
+
+    eval_strategy="epoch",
+
+    learning_rate=2e-5,
+
+    per_device_train_batch_size=16,
+
+    per_device_eval_batch_size=16,
+
+    weight_decay=0.01,
+
+    save_total_limit=3,
+
+    num_train_epochs=4,
+
+    predict_with_generate=True,
+
+    fp16=True, #change to bf16=True for XPU
+
+    push_to_hub=True,
+
+)
+
+
+trainer = Seq2SeqTrainer(
+
+    model=model,
+
+    args=training_args,
+
+    train_dataset=tokenized_billsum["train"],
+
+    eval_dataset=tokenized_billsum["test"],
+
+    processing_class=tokenizer,
+
+    data_collator=data_collator,
+
+    compute_metrics=compute_metrics,
+
+)
+
+trainer.train()
+
