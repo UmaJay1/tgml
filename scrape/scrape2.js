e4541659e30ebb70415623de52a03ed2a553b158
OmarJay1 <43353780+OmarJay1@users.noreply.github.com>
2025-07-18 07:44:31 -0700

    Work on scrape2.js

--- scrape/scrape2.js
+++ scrape/scrape2.js
@@ -1,8 +1,23 @@
 const axios = require('axios');
 const { convert } = require('html-to-text');
 
+function getTextSection(props) {
+
+
+    return;
+}
+
+function getTextSections(props) {
+
+    
+
+
+}
+
 (async () => {
-    const response = await axios.get('https://pubmed.ncbi.nlm.nih.gov/8839957/');
+//    const response = await axios.get('https://pubmed.ncbi.nlm.nih.gov/8839957/');
+    const response = await axios.get('https://pubmed.ncbi.nlm.nih.gov/31740479/');
+
     const text = convert(response.data);
 
     console.log(text);
